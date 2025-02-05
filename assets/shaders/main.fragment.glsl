231ccbfdcb83b773acb0064d8f76e7ab6422efce
Minuta18 <ig.samsonov10@yandex.ru>
2025-02-05 19:33:29 +0300

    added meterials

--- assets/shaders/main.fragment.glsl
+++ assets/shaders/main.fragment.glsl
@@ -1,29 +1,52 @@
 #version 330 core
 out vec4 FragColor;
 
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    vec3 color;
+    float shininess;
+}
+
+struct Light {
+    vec3 pos;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+}
+
 in vec3 Normal;  
 in vec3 FragPos;  
   
-uniform vec3 lightPos; 
+uniform Material material;
+uniform Material light;
 uniform vec3 viewPos; 
-uniform vec3 lightColor;
-uniform vec3 objectColor;
+
+vec3 calcAmbientLight() {
+    return light.ambient * material.ambient;
+}
+
+vec3 calcDiffuseLight() {
+    vec3 normal = normalize(Normal);
+    vec3 lightDir = normalize(light.pos - FragPos);
+    float diff = max(dot(norm, lightDit), 0.0);
+    return light.diffuse * (diff * material.diffuse);
+}
+
+vec3 calcSpecularLight() {
+    vec3 lightDir = normalize(light.pos - FragPos);
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    return light.specular * (spec * material.specular);
+}
 
 void main() {
-    float ambientStrength = 0.1;
-    vec3 ambient = ambientStrength * lightColor;
-  	
-    vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
-    
-    float specularStrength = 0.5;
-    vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);  
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = specularStrength * spec * lightColor;  
+    vec3 ambient = calcAmbientLight();
+    vec3 diffuse = calcDiffuseLight();
+    vec3 specular = calcSpecularLight();
         
-    vec3 result = (ambient + diffuse + specular) * objectColor;
+    vec3 result = (ambient + diffuse + specular) * material.color;
     FragColor = vec4(result, 1.0);
 } 
