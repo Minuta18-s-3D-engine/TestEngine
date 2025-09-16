3f0e8bf38e266b47cdbff776b8c847d5f2e00ccd
Minuta18 <ig.samsonov10@yandex.ru>
2025-09-16 07:58:42 +0300

    Mesh done without draw()

---
+++ src/engine/models/Mesh.hpp
@@ -0,0 +1,29 @@
+#ifndef ENGINE_MODELS_MESH_H_
+#define ENGINE_MODELS_MESH_H_
+
+#include "glm/glm.hpp"
+#include <vector>
+#include "../utils/EngineTypes.h"
+#include "../materials/TextureMaterial.hpp"
+#include "../graphics/Shader.hpp"
+
+struct Vertex {
+    glm::vec3 pos, normal;
+    glm::vec2 texCords;
+};
+
+class Mesh {
+    uint VAO, VBO, EBO;
+
+    void setupMesh();
+public:
+    std::vector<Vertex> vertices;
+    std::vector<uint> indices;
+    std::vector<TextureMaterial> textures;
+
+    Mesh(std::vector<Vertex> _vertices, std::vector<uint> _indices, 
+        std::vector<TextureMaterial> _textures);
+    void draw(Shader& shader);
+};
+
+#endif // ENGINE_MODELS_MESH_H_
