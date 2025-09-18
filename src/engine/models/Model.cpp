83850161c9a4c134539c065c5d4706147dbd7665
Minuta18 <ig.samsonov10@yandex.ru>
2025-09-18 15:09:53 +0300

    Model class. Yet no actual loading, I'm going to move this code later

---
+++ src/engine/models/Model.cpp
@@ -0,0 +1,32 @@
+#include "Model.hpp"
+
+Model::Model(std::vector<std::shared_ptr<Mesh>> meshes) {
+    meshes = meshes;
+    wasLoaded = false;
+    filename = "";
+}
+
+Model::Model(std::string filename) {
+    filename = filename;
+    wasLoaded = true;
+
+    this->loadModel(filename);
+}
+
+void Model::loadModel(std::string filename) {
+    // TODO
+}
+
+void Model::processNode(aiNode* node, const aiScene* scene) {
+    // TODO
+}
+
+Mesh Model::processMesh(aiMesh* mesh, const aiScene* scene) {
+    // TODO
+}
+
+std::vector<TextureMaterial> Model::loadMaterialTextures(
+    aiMaterial* mat, aiTextureType type, std::string typeName
+) {
+    // TODO
+}
--- src/engine/models/Model.hpp
+++ src/engine/models/Model.hpp
@@ -1,18 +1,30 @@
 #ifndef ENGINE_MODELS_MODEL_H_
 #define ENGINE_MODELS_MODEL_H_
 
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 #include <vector>
 #include <string>
+#include <memory>
 
 #include "Mesh.hpp"
 
 class Model {
-    std::vector<Mesh> meshes;
+    std::vector<std::shared_ptr<Mesh>> meshes;
     std::string filename;
-
+    bool wasLoaded;
     
+    void loadModel(std::string filename);
+    void processNode(aiNode* node, const aiScene* scene);
+    Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+    std::vector<TextureMaterial> loadMaterialTextures(
+        aiMaterial* mat, aiTextureType type, std::string typeName
+    );
 public:
-    Model(/* args */);
+    Model(std::vector<std::shared_ptr<Mesh>> meshes);
+    Model(std::string filename);
 };
 
 #endif // ENGINE_MODELS_MODEL_H_
