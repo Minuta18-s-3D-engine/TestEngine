bc689a9387317078b5934c0850464aec2e370e17
Minuta18 <ig.samsonov10@yandex.ru>
2025-10-04 17:18:36 +0300

    GBuffer class complete (WIP)

---
+++ src/engine/graphics/GBuffer.cpp
@@ -0,0 +1,96 @@
+#include "GBuffer.hpp"
+
+GBuffer::GBuffer(uint _width, uint _height) : width(_width), height(_height) {
+    createFBO();
+    createBuffers();
+
+    GLenum attachments[BufferType::NUM_BUFFERS] = {
+        GL_COLOR_ATTACHMENT0,
+        GL_COLOR_ATTACHMENT1,
+        GL_COLOR_ATTACHMENT2
+    };
+    glDrawBuffers(BufferType::NUM_BUFFERS, attachments);
+  
+    createDepthBuffer();
+
+    int status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+    if (status != GL_FRAMEBUFFER_COMPLETE) {
+        throw std::runtime_error("GBuffer is not complete: " + status);
+    }
+
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+GBuffer::~GBuffer() {
+    glDeleteFramebuffers(1, &fboId);
+    glDeleteRenderbuffers(1, &depthBufferId);
+    for (int i = 0; i < BufferType::NUM_BUFFERS; ++i) {
+        glDeleteTextures(1, &buffersId[i]);
+    }
+}
+
+void GBuffer::createFBO() {
+    glGenFramebuffers(1, &fboId);
+    glBindFramebuffer(GL_FRAMEBUFFER, fboId);
+}
+
+void GBuffer::createBuffer(uint ind, GLuint format, GLenum type) {
+    if (!buffersInitialized)
+        glGenTextures(1, &buffersId[ind]);
+    glBindTexture(GL_TEXTURE_2D, buffersId[ind]);
+    glTexImage2D(
+        GL_TEXTURE_2D, 0, format, width, height, 0, GL_RGBA, type, NULL
+    );
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(
+        GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + ind, GL_TEXTURE2, 
+        buffersId[ind], 0
+    );
+}
+
+void GBuffer::createDepthBuffer() {
+    glBindRenderbuffer(GL_RENDERBUFFER, depthBufferId);
+    glRenderbufferStorage(
+        GL_RENDERBUFFER, GL_DEPTH_COMPONENT, width, height
+    );
+    glFramebufferRenderbuffer(
+        GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthBufferId
+    );
+}
+
+void GBuffer::createBuffers() {
+    createBuffer(0, GL_RGBA16F, GL_FLOAT);
+    createBuffer(0, GL_RGBA16F, GL_FLOAT);
+    createBuffer(0, GL_RGBA, GL_UNSIGNED_BYTE);
+    buffersInitialized = true;
+}
+
+void GBuffer::bind() {
+    glBindFramebuffer(GL_FRAMEBUFFER, fboId);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void GBuffer::unbind() {
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+void GBuffer::bindBufffers() {
+    for (int i = 0; i < BufferType::NUM_BUFFERS; ++i) {
+        glActiveTexture(GL_TEXTURE0 + i);
+        glBindTexture(GL_TEXTURE_2D, buffersId[i]);
+    }
+}
+
+void GBuffer::resize(uint _width, uint _height) {
+    if (width == _width && height == _height) return;
+    this->width = width;
+    this->height = height;
+
+    glBindFramebuffer(GL_FRAMEBUFFER, fboId);
+
+    createBuffers();
+
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
--- src/engine/graphics/GBuffer.hpp
+++ src/engine/graphics/GBuffer.hpp
@@ -1,18 +1,41 @@
 #ifndef ENGINE_GRAPHICS_GBUFFER_H
 #define ENGINE_GRAPHICS_GBUFFER_H
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <exception>
+#include <stdexcept>
+
 #include "../utils/EngineTypes.h"
 
 class GBuffer {
-    uint width, height;
-
-    void createBuffers();
 public:
+    enum BufferType {
+        BUFFER_POSITION,
+        BUFFER_ALBEDO,
+        BUFFER_NORMAL,
+        NUM_BUFFERS
+    };
+
     GBuffer(uint _width, uint _height);
     virtual ~GBuffer();
+    void resize(uint _width, uint _height);
 
-    void bindForWriting();
-    void bindForReading();
-}
+    void bind();
+    void unbind();
+    void bindBufffers();
+private:
+    uint width, height;
+    bool buffersInitialized = false;
+    uint buffersId[BufferType::NUM_BUFFERS];
+    uint fboId;
+    uint depthBufferId;
+
+    void createFBO();
+    void createBuffer(uint ind, GLuint format, GLenum type);
+    void createDepthBuffer();
+    void createBuffers();
+};
 
 #endif // ENGINE_GRAPHICS_GBUFFER_H
