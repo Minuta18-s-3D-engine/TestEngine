9dd8d5f8d6ddac38a7669c51ea382fcfae01deff
Minuta18 <ig.samsonov10@yandex.ru>
2025-10-06 18:23:52 +0300

    The new approach found: Clustered rendering! Some computation shaders done using DaveH355's tutorial: https://github.com/DaveH355/clustered-shading

---
+++ assets/shaders/buildClusters.comp.glsl
@@ -0,0 +1,87 @@
+#version 430 core
+
+#define MATH_EPS 0.00001
+#define LIGHTS_PRE_CLUSTER_LIMIT 100
+
+layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
+
+struct Cluster {
+    vec4 minPoint;
+    vec4 maxPoint;
+    uint count;
+    uint lightIndices[LIGHTS_PRE_CLUSTER_LIMIT];
+};
+
+layout(std430, binding = 1) restrict buffer clusterSSBO {
+    Cluster clusters[];
+};
+
+uniform float zNear;
+uniform float zFar;
+uniform mat4 inverseProjection;
+uniform uvec3 gridSize;
+uniform uvec2 screenDimensions;
+
+vec3 screenToView(vec2 screenCoord);
+vec3 lineIntersectionWithZPlane(
+    vec3 startPoint, 
+    vec3 endPoint, 
+    float zDistance
+);
+
+vec3 screenToView(vec2 screenCoord) {
+    vec4 ndc = vec4(screenCoord / screenDimensions * 2.0 - 1.0, -1.0, 1.0);
+
+    vec4 viewCoord = inverseProjection * ndc;
+    viewCoord /= viewCoord.w;
+    return viewCoord.xyz;
+}
+
+vec3 lineIntersectionWithZPlane(
+    vec3 startPoint, 
+    vec3 endPoint, 
+    float zDistance
+) {
+    vec3 direction = endPoint - startPoint;
+
+    if (abs(direction.z) < MATH_EPS) {
+        return startPoint;
+    }
+
+    float t = (zDistance - startPoint.z) / direction.z;
+    return startPoint + t * direction;
+}
+
+void main() {
+    uint tileIndex = gl_WorkGroupID.x + (gl_WorkGroupID.y * gridSize.x) +
+            (gl_WorkGroupID.z * gridSize.x * gridSize.y);
+    
+    vec2 tileSize = vec2(screenDimensions) / vec2(gridSize.xy);
+
+    vec2 minTile_screenspace = gl_WorkGroupID.xy * tileSize;
+    vec2 maxTile_screenspace = (gl_WorkGroupID.xy + 1) * tileSize;
+
+    vec3 minTile = screenToView(minTile_screenspace);
+    vec3 maxTile = screenToView(maxTile_screenspace);
+
+    // Exponential formula by DaveH355
+    // https://github.com/DaveH355/clustered-shading
+    float planeNear = zNear * pow(zFar / zNear, 
+        gl_WorkGroupID.z / float(gridSize.z));
+    float planeFar = zNear * pow(zFar / zNear, (gl_WorkGroupID.z + 1) / 
+        float(gridSize.z));
+
+    // Points calculation
+    vec3 minPointNear = lineIntersectionWithZPlane(
+        vec3(0, 0, 0), minTile, planeNear);
+    vec3 minPointFar = lineIntersectionWithZPlane(
+        vec3(0, 0, 0), minTile, planeFar);
+    vec3 maxPointNear = lineIntersectionWithZPlane(
+        vec3(0, 0, 0), maxTile, planeNear);
+    vec3 maxPointFar = lineIntersectionWithZPlane(
+        vec3(0, 0, 0), maxTile, planeFar);
+
+    clusters[tileIndex].minPoint = vec4(min(minPointNear, minPointFar), 0.0);
+    clusters[tileIndex].maxPoint = vec4(max(maxPointNear, maxPointFar), 0.0);
+    clusters[tileIndex].count = 0; 
+}
---
+++ assets/shaders/lightCulling.comp.glsl
@@ -0,0 +1,78 @@
+#version 430 core
+
+#define LOCAL_SIZE 128
+#define LIGHTS_PRE_CLUSTER_LIMIT 100
+
+layout(local_size_x = LOCAL_SIZE, local_size_y = 1, local_size_z = 1) in;
+
+struct PointLight {
+    vec4 position; // xyz - actual position
+                   // w   - radius
+    vec4 color;
+};
+
+struct Cluster {
+    vec4 minPoint;
+    vec4 maxPoint;
+    uint count;
+    uint lightIndices[100];
+};
+
+layout (std430, binding = 1) restrict buffer clusterSSBO {
+    Cluster clusters[];
+}
+
+layout (std430, binding = 2) restrict buffer lightSSBO {
+    PointLight lights[];
+}
+
+uniform mat4 viewMat;
+uniform uint numLights;
+
+bool sphereAABBIntersection(vec3, float, vec2, vec3);
+bool testSphereAABB(uint, uint);
+
+bool sphereAABBIntersection(
+    vec3 center, 
+    float radius, 
+    vec3 aabbMin, 
+    vec3 aabbMax
+) {
+    vec3 closestPoint = max(aabbMax, min(center, aabbMin));
+
+    float distanceSquared = dot(closestPoint - center, closestPoint - center);
+    return distanceSquared <= (radius * radius);
+}
+
+bool testSphereAABB(uint lightInd, Cluster currCluster) {
+    vec4 lightViewPos = viewMat * vec4(lights[lightInd].position.xyz, 1.0);
+    vec3 center = lightViewPos.xyz;
+    float radius = lights[lightInd].position.w;
+
+    vec3 aabbMin = currCluster.minPoint.xyz;
+    vec3 aabbMax = currCluster.maxPoint.xyz;
+
+    return sphereAABBIntersection(center, radius, aabbMin, aabbMax);
+}
+
+void main() {
+    uint clusterInd = gl_WorkGroupID.x * LOCAL_SIZE + gl_LocalInvocationID.x;
+
+    uint totalClusters = gridSize.x * gridSize.y * gridSize.z;
+    if (clusterInd >= totalClusters) {
+        return;
+    }
+
+    Cluster currCluster = clusters[clusterInd];
+
+    currCluster.count = 0;
+    for (uint i = 0; i < numLights; ++i) {
+        if (testSphereAABB(i, currCluster) && 
+            currCluster.count < LIGHTS_PRE_CLUSTER_LIMIT) {
+            currCluster.lightIndices[currCluster.count] = i;
+            currCluster.count++;
+        }
+    }
+
+    clusters[clusterInd] = currCluster;
+}
