3e3e776ed86be8fe2b942bc9176b956fbfdb447b
Minuta18 <ig.samsonov10@yandex.ru>
2025-10-16 22:58:11 +0300

    Removed unnecessary debugging code

--- assets/shaders/lightingShader.fragment.glsl
+++ assets/shaders/lightingShader.fragment.glsl
@@ -33,11 +33,11 @@ struct PointLight {
     float radius;
 };
 
-layout (std430, binding = 0) restrict coherent buffer clusterSSBO {
+layout (std430, binding = 0) restrict buffer clusterSSBO {
     Cluster clusters[];
 };
 
-layout (std430, binding = 1) restrict coherent buffer lightSSBO {
+layout (std430, binding = 1) restrict buffer lightSSBO {
     PointLight lights[];
 };
 uniform int numLights;
@@ -54,6 +54,54 @@ in vec3 FragPos;
 in vec2 TexCoords;
 in vec3 ViewSpacePos;
 
+uniform Material mainMaterial;
+
+uniform TextureMaterial textureDiffuse1Mat;
+uniform TextureMaterial textureSpecular1Mat;
+uniform sampler2D textureDiffuse1;
+uniform sampler2D textureSpecular1;
+
+vec3 getDiffTexturePixel(vec2 coords) {
+    if (!textureDiffuse1Mat.isActive) return vec3(0.0, 0.0, 0.0);
+    return vec3(texture(textureDiffuse1, coords));
+}
+
+vec3 getSpecTexturePixel(vec2 coords) {
+    if (!textureSpecular1Mat.isActive) return vec3(0.0, 0.0, 0.0);
+    return vec3(texture(textureSpecular1, coords));
+}
+
+const float AMBIENT_LIGHT = 0.3;
+
+vec3 calcAmbientLight(vec2 coords) {
+    return texture(textureDiffuse1, coords).rgb * AMBIENT_LIGHT;
+}
+
+vec3 calcDiffuseLight(uint lightId, vec2 coords) {
+    vec3 Diffuse = texture(textureDiffuse1, coords).rgb;
+    vec3 lightDir = normalize(lights[lightId].position - FragPos);
+    return max(dot(Normal, lightDir), 0.0) * Diffuse * lights[lightId].color;
+}
+
+vec3 calcSpecularLight(uint lightId, vec2 coords) {
+    float Specular = texture(textureSpecular1, coords).r;
+    vec3 viewDir  = normalize(ViewPos - FragPos);
+    vec3 lightDir = normalize(lights[lightId].position - FragPos);
+    vec3 halfwayDir = normalize(lightDir + viewDir);  
+    float spec = pow(max(dot(Normal, halfwayDir), 0.0), 16.0); // shinessness 
+            // will go here
+    vec3 specular = lights[lightId].color * spec * Specular;
+    return specular;
+}
+
+float calcAttenuation(uint lightId, float distToLightSource) {
+    float linearPart = lights[lightId].linear * distToLightSource;
+    float quadraticPart = lights[lightId].quadratic * distToLightSource 
+        * distToLightSource;
+    float attenuation = 1.0 / (0.7 + linearPart + quadraticPart);
+    return attenuation;
+}
+
 uint findClusterIndex() {
     uvec2 tileSize = screenDimensions / gridSize.xy;
     uvec2 tileXY = uvec2(gl_FragCoord.xy) / tileSize;
@@ -66,6 +114,10 @@ uint findClusterIndex() {
     return tileXY.x + tileXY.y * gridSize.x + tileZ * gridSize.x * gridSize.y;
 }
 
+float rand(vec2 co){
+    return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
+}
+
 void main() {
     uint clusterIndex = findClusterIndex();
     Cluster currCluster = clusters[clusterIndex];
@@ -75,7 +127,20 @@ void main() {
         return;
     }
 
-    if (currCluster.count > 0) {
-        FragColor = vec4(lights[0].color.r, lights[0].color.g, lights[0].color.b, 1.0);
+    vec3 result = calcAmbientLight(TexCoords);
+    for (int i = 0; i < currCluster.count; ++i) {
+        uint lightIndex = currCluster.lightIndices[i];
+
+        float dist = length(lights[lightIndex].position - FragPos);
+
+        if (dist < lights[lightIndex].radius) {
+            vec3 diffuse = calcDiffuseLight(lightIndex, TexCoords);
+            vec3 specular = calcSpecularLight(lightIndex, TexCoords);
+            float attenuation = calcAttenuation(lightIndex, dist);
+
+            result += (diffuse + specular) * attenuation;
+        }
     }
+
+    FragColor = vec4(result, 1.0); 
 }
--- src/engine/scene/Scene.cpp
+++ src/engine/scene/Scene.cpp
@@ -37,76 +37,8 @@ void Scene::renderQuad() {
 }
 
 void Scene::drawAll(Camera* cam) {
-    // Shader& mainShader = assetManager.require<Shader>("shaders/forward/main");
-
-    // glm::mat4 proj = glm::mat4(1.0f);
-    // proj = glm::perspective(
-    //     cam->getZoom(), 
-    //     (float) Window::width / (float) Window::height, 
-    //     0.1f, 100.0f
-    // );
-
-
-    // glm::mat4 viewMat = cam->getViewMat();
-    // glm::mat4 worldModel = glm::mat4(1.0f);
-    // for (auto& object : this->objects) {
-    //     glm::mat4 model = glm::translate(worldModel, object.get()->position);
-        
-    //     mainShader.use();
-    //     mainShader.setUniform4mat("projection", proj);
-    //     mainShader.setUniform4mat("view", viewMat);
-    //     mainShader.setUniform3f("viewPos", cam->pos);
-    //     mainShader.setUniform4mat("model", model);
-
-    //     lights[0]->passToShader(mainShader, "light");
-    //     object->draw(mainShader);
-    // }
-
-    // Shader& geomShader = assetManager.require<Shader>("shaders/geomShader");
-    // Shader& lightingShader = assetManager.require<Shader>("shaders/lightingShader");
-
-    // // gBuffer->bind();
-
-    // glm::mat4 proj = glm::mat4(1.0f);
-    // proj = glm::perspective(
-    //     cam->getZoom(), 
-    //     (float) Window::width / (float) Window::height, 
-    //     cam->zNear, cam->zFar
-    // );
-    // glm::mat4 viewMat = cam->getViewMat();
-    // glm::mat4 worldModel = glm::mat4(1.0f);
-
-    // geomShader.use();
-    // geomShader.setUniform4mat("projection", proj);
-    // geomShader.setUniform4mat("view", viewMat);
-    // for (auto& object : this->objects) {
-    //     glm::mat4 model = glm::translate(worldModel, object.get()->position);
-        
-    //     geomShader.setUniform4mat("model", model);
-    //     object->draw(geomShader);
-    // }
-
-    // // gBuffer->unbind();
-
-    // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    // lightingShader.use();
-    // // gBuffer->bindBufffers();
-    // lightingShader.setUniform1i("gPosition", 0);
-    // lightingShader.setUniform1i("gNormal", 1);
-    // lightingShader.setUniform1i("gAlbedoSpec", 2);
-    // lightingShader.setUniform3f("viewPos", cam->pos);
-    // lightingShader.setUniform1i("lights_size", this->lights.size());
-    // for (int i = 0; i < this->lights.size(); ++i) {
-    //     auto light = this->lights[i];
-    //     light->passToShader(lightingShader, "lights", i);
-    // }
-    // renderQuad();
-
-    renderer->bindClusterData();
     renderer->updateLightData(this->lights);
-    renderer->bindClusterData();
     renderer->updateClusters(cam);
-    renderer->bindClusterData();
 
     Shader& lightingShader = assetManager.require<Shader>("shaders/lightingShader"); 
     lightingShader.use();
