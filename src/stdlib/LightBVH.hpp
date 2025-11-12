b247aee74a3831b02bdb626a0d5dcefe0c0c357d
Minuta18 <ig.samsonov10@yandex.ru>
2025-11-12 23:37:58 +0300

    LightBVHNode structure

--- src/stdlib/LightBVH.hpp
+++ src/stdlib/LightBVH.hpp
@@ -1,10 +1,21 @@
 #ifndef ENGINE_STDLIB_LIGHTBVH_H_
 #define ENGINE_STDLIB_LIGHTBVH_H_
 
+#include "glm/glm.hpp"
+
 #include <memory>
 #include <vector>
 
 struct CompLight;
+
+struct alignas(16) LightBVHNode {
+    alignas(16) glm::vec3 AABBp1;
+    alignas(16) glm::vec3 AABBp2;
+    uint leftChildIndex;
+    uint rightChildIndex;
+    uint lightIndex;
+    bool isLeaf;
+};
 
 class LightBVH {
 public:
