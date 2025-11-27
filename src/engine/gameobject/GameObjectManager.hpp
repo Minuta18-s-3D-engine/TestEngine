3c13ae2eeb1e496256c9c84176b1e93751f4b84d
Minuta18 <ig.samsonov10@yandex.ru>
2025-11-27 07:58:00 +0300

    WIP: GameObjectManager

---
+++ src/engine/gameobject/GameObjectManager.hpp
@@ -0,0 +1,10 @@
+#ifndef ENGINE_GAMEOBJECT_GAMEOBJECTMANAGER_H_
+#define ENGINE_GAMEOBJECT_GAMEOBJECTMANAGER_H_
+
+class GameObjectManager {
+public:
+    GameObjectManager();
+    ~GameObjectManager();
+};
+
+#endif // ENGINE_GAMEOBJECT_GAMEOBJECTMANAGER_H_
