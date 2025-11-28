3c11e4bc8d548fb22c5eb95a598ba91676a0533c
Minuta18 <ig.samsonov10@yandex.ru>
2025-11-28 08:35:39 +0300

    WIP: GameObjectManager 2

--- src/engine/gameobject/GameObjectManager.hpp
+++ src/engine/gameobject/GameObjectManager.hpp
@@ -1,10 +1,24 @@
 #ifndef ENGINE_GAMEOBJECT_GAMEOBJECTMANAGER_H_
 #define ENGINE_GAMEOBJECT_GAMEOBJECTMANAGER_H_
 
+#include <uuid.h>
+
+#include "GameObject.hpp"
+
+#include <vector>
+#include <unordered_map>
+
 class GameObjectManager {
+private:
+    std::unordered_map<uuids::uuid, std::unique_ptr<GameObject>> objects;
+    std::vector<GameObject*> staticObjects;
+    std::vector<GameObject*> dynamicObjects;
+
 public:
     GameObjectManager();
     ~GameObjectManager();
+
+
 };
 
 #endif // ENGINE_GAMEOBJECT_GAMEOBJECTMANAGER_H_
