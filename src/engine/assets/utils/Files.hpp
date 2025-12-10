0ce935555e21aecc879bc70a274c6d75b75c7eed
Igor Samsonov <ig.samsonov10@yandex.ru>
2025-12-10 10:20:04 +0300

    getImageType util

--- src/engine/assets/utils/Files.hpp
+++ src/engine/assets/utils/Files.hpp
@@ -6,11 +6,20 @@
 #include <fstream>
 #include <stdexcept>
 #include <vector>
+#include <cstring>
 
 namespace fs = std::filesystem;
 
 std::unique_ptr<uint8_t[]> read_bytes(
     const fs::path& filename, size_t& length
 );
+
+enum ImageType {
+    PNG,
+    JPG,
+    UNKNOWN
+};
+
+ImageType getImageType(std::unique_ptr<uint8_t[]> imageData, size_t length);
 
 #endif // ENGINE_ASSETS_UTILS_FILES_H_
