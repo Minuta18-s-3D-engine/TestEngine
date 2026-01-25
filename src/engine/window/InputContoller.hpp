f72992188bd0c3e78e907d6fab252b81aa57a757
Minuta18 <ig.samsonov10@yandex.ru>
2026-01-25 21:48:54 +0300

    InputController.hpp minor update

--- src/engine/window/InputContoller.hpp
+++ src/engine/window/InputContoller.hpp
@@ -4,6 +4,8 @@
 #include <cstring>
 #include <stdint.h>
 
+#include "glm/glm.hpp"
+
 class InputController {
     const static int KEYBOARD_BUTTONS = 1024;
     const static int MOUSE_BUTTONS = 32;
@@ -11,8 +13,24 @@ class InputController {
 
     bool pressed[KEY_BUFFER_SIZE];
     uint32_t frames[KEY_BUFFER_SIZE];
+
+    glm::vec2 mousePosition;
+    glm::vec2 lastMousePosition;
+
+    bool isCursorPinned;
 public:
     InputController();
+
+    glm::vec2 getMousePosition() const;
+    glm::vec2 getMouseMovement() const;
+
+    bool isKeyPressed(int keyCode) const;
+    bool isKeyJustPressed(int keyCode) const;
+    bool isMousePressed(int keyCode) const;
+    bool isMouseJustPressed(int keyCode) const;
+
+    bool isCursorPinned() const;
+    void pinCursor(bool shouldPin) const;
 };
 
 #endif // ENGINE_WINDOW_INPUTCONTROLLER_H_
