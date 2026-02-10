e9b7826a11feeccd809320fa92c583728911d13b
Minuta18 <ig.samsonov10@yandex.ru>
2026-02-10 22:04:47 +0300

    Project loader done. Very dirty yet, I will create separate config system later

--- src/engine/project/ProjectLoader.cpp
+++ src/engine/project/ProjectLoader.cpp
@@ -1,7 +1,41 @@
 #include "ProjectLoader.hpp"
 
+nlohmann::json ProjectLoader::readJsonFile(const std::ifstream& fileStream) {
+    try {
+        nlohmann::json result = nlohmann::json::parse(fileStream);
+        return result;
+    } catch (const nlohmann::json::parse_error& e) {
+        throw exc::parse_error(e.what());
+    }
+}
+
 std::unique_ptr<Project> ProjectLoader::loadProject(
-    const std::filesystem::path& path
+    const std::filesystem::path& folderPath
 ) {
-    
+    std::filesystem::path configPath = folderPath / projectConfigFilename;
+    if (!std::filesystem::exists(configPath)) {
+        throw exc::file_not_found(std::format(
+            "Project config file {} not found.",
+            configPath
+        ));
+    }
+
+    std::ifstream configFile(configPath);
+    nlohmann::json config = readJsonFile(configFile);
+
+    if (!config.contains("name")) {
+        throw exc::validation_error("Mandatory field \"name\" not found.");
+    }
+
+    if (!config.contains("projectVersion")) {
+        throw exc::validation_error(
+            "Mandatory field \"projectVersion\" not found.");
+    }
+
+    std::unique_ptr<Project> project = std::make_unique<Project>(
+        config["name"],
+        config["projectVersion"]
+    );
+
+    return project;
 }
--- src/engine/project/ProjectLoader.hpp
+++ src/engine/project/ProjectLoader.hpp
@@ -4,12 +4,22 @@
 #include <json.hpp>
 
 #include <filesystem>
+#include <string>
+#include <stdexcept>
+#include <format>
+#include <fstream>
 
 #include "Project.hpp"
+#include "../utils/exc/ParseExceptions.hpp"
 
 class ProjectLoader {
+    std::string projectConfigFilename = "project.json";
+
+    nlohmann::json readJsonFile(const std::ifstream& fileStream);
 public:
-    std::unique_ptr<Project> loadProject(const std::filesystem::path& path);
+    std::unique_ptr<Project> loadProject(
+        const std::filesystem::path& folderPath
+    );
 };
 
 #endif // ENGINE_PROJECT_PROJECTLOADER_HPP_
