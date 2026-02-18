313a4745f6ea544a63d3a0ae330414e1c6ebbc59
Minuta18 <ig.samsonov10@yandex.ru>
2026-02-18 21:45:45 +0300

    Material interface update

--- src/engine/materials/Material.cpp
+++ src/engine/materials/Material.cpp
@@ -1,5 +1,10 @@
 #include "Material.hpp"
 
-Material::PropertyMap& Material::getPropertyMap() {
-    return properties;
-}
+Material::Material(
+    const std::string& _name, std::shared_ptr<Shader> _shader
+) : name(_name), shader(_shader) {}
+
+const std::string& Material::getName() const { return name; }
+std::shared_ptr<Shader> Material::getShader() const { return shader; }
+const TypedPropertyStorage& Material::getPropertyStorage() { return properties; }
+const Material::TextureStorage& Material::getTextureStorage() { return textures; }
--- src/engine/materials/Material.hpp
+++ src/engine/materials/Material.hpp
@@ -9,23 +9,54 @@
 #include "TypedPropertyStorage.hpp"
 
 class Texture;
+class Shader;
+
+template <typename T>
+struct MaterialTypeTraits {
+    using ArgType = const T&;
+};
+
+template <>
+struct MaterialTypeTraits<Texture> {
+    using ArgType = std::shared_ptr<Texture>;
+};
 
 class Material {
-    using TextureStorage = std::unordered_map<std::string, std::shared_ptr<Texture>>;
+    using TextureStorage = std::unordered_map<
+        std::string, std::shared_ptr<Texture>
+    >;
+
+    std::shared_ptr<Shader> shader;
+    std::string name;
 private:
-    TypedPropertyStorage& properties;
+    TypedPropertyStorage properties;
     TextureStorage textures;
 public:
-    Material() = default;
+    Material(const std::string& _name, std::shared_ptr<Shader> _shader);
 
     template <typename T>
-    Material& setProperty(std::string& name, const T& value);
+    Material& setProperty(
+        std::string& name, typename MaterialTypeTraits<T>::ArgType value
+    );
 
     template <typename T>
     Material& setProperty(std::string& name);
 
     template <typename T>
-    const T& getProperty(std::string& name);
+    Material& addProperty(
+        std::string& name, typename MaterialTypeTraits<T>::ArgType value
+    );
+
+    template <typename T>
+    Material& addProperty(std::string& name);
+
+    template <typename T>
+    const T& getProperty(std::string& name) const;
+
+    const std::string& getName() const;
+    std::shared_ptr<Shader> getShader() const;
+    const TypedPropertyStorage& getPropertyStorage();
+    const TextureStorage& getTextureStorage();
 };
 
 #endif // ENGINE_MATERIALS_MATERIAL_H_
