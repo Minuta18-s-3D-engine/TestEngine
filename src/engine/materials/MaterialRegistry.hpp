0fb42c5b47651d2c7a45d0be9b7507a4fb489ab8
Minuta18 <ig.samsonov10@yandex.ru>
2026-02-22 20:19:36 +0300

    WIP: MaterialRegistry

---
+++ src/engine/materials/MaterialRegistry.hpp
@@ -0,0 +1,23 @@
+#ifndef ENGINE_MATERIALS_MATERIALINSTANCE_H_
+#define ENGINE_MATERIALS_MATERIALINSTANCE_H_
+
+#include "Material.hpp"
+
+class MaterialInstance;
+
+#include "MaterialInstance.hpp"
+
+class MaterialRegistry {
+public:
+    using MaterialID = size_t;
+    using MaterialInstanceID = size_t;
+public:
+    MaterialRegistry() = default;
+
+    MaterialID registerMaterial(const Material& material);
+    MaterialInstanceID registerMaterialInstance(
+        const MaterialInstance& instance
+    );
+};
+
+#endif // ENGINE_MATERIALS_MATERIALINSTANCE_H_
