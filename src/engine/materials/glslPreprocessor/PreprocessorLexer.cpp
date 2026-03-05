32ad90a8b146755833dd5120cdfab572d214799f
Minuta18 <ig.samsonov10@yandex.ru>
2026-03-05 20:24:34 +0300

    major lexer update

--- src/engine/materials/glslPreprocessor/PreprocessorLexer.cpp
+++ src/engine/materials/glslPreprocessor/PreprocessorLexer.cpp
@@ -17,6 +17,10 @@ void PreprocessorLexer::advance() {
     cursorPosition++;
 }
 
+void PreprocessorLexer::advance(int n) {
+    for (int i = 0; i < n; ++i) advance();
+}
+
 bool PreprocessorLexer::in(char c, const std::string& str) {
     for (char s : str) {
         if (s == c) return true;
@@ -41,6 +45,94 @@ bool PreprocessorLexer::isAlphaDigit(char c) {
     return isAlpha(c) || isDigit(c);
 }
 
-PreprocessorLexer::Token PreprocessorLexer::nextToken() {
+PreprocessorLexer::Token PreprocessorLexer::handleCodeState() {
+    std::string_view lastPart = source.substr(cursorPosition);
 
+    size_t markerPos = lastPart.find(directiveMarker);
+
+    Token resultingToken;
+    if (markerPos == 0) {
+        resultingToken = {
+            .type = PreprocessorLexer::TokenType::DirectiveMarker,
+            .value = directiveMarker,
+            .line = line,
+            .column = column, 
+            .position = cursorPosition
+        };
+        currentState = State::Directive;
+        advance(directiveMarker.size());
+    } else {
+        std::size_t codeLen = markerPos;
+        if (markerPos == std::string_view::npos) codeLen = lastPart.size();
+        resultingToken = {
+            .type = PreprocessorLexer::TokenType::Code,
+            .value = lastPart.substr(0, codeLen),
+            .line = line,
+            .column = column, 
+            .position = cursorPosition
+        };
+        advance(codeLen);
+    }
+
+    return resultingToken;
+}
+
+PreprocessorLexer::Token PreprocessorLexer::handleDirectiveState() {
+    std::string lastPart(source.substr(cursorPosition));
+
+    for (const auto& rule : directiveRules) {
+        std::smatch match;
+        if (std::regex_search(lastPart, match, rule.pattern)) {
+            Token resultingToken {
+                .type = rule.type,
+                .value = match.str(),
+                .line = line,
+                .column = column, 
+                .position = cursorPosition
+            };
+            advance(match.str().size());
+            return resultingToken;
+        }
+    }
+
+    Token resultingToken {
+        .type = PreprocessorLexer::TokenType::Unknown,
+        .value = source.substr(cursorPosition, unknownTokenSize),
+        .line = line,
+        .column = column, 
+        .position = cursorPosition
+    };
+    advance(unknownTokenSize);
+    return resultingToken;
+}
+
+PreprocessorLexer::Token PreprocessorLexer::nextToken() {
+    if (cursorPosition >= source.size()) {
+        return EOFToken;
+    } 
+
+    if (currentState == State::Code) {
+        return handleCodeState();
+    } else if (currentState == State::Directive) {
+        while (cursorPosition < source.size() && in(peek(), " \t\r")) {
+            advance();
+        }
+
+        if (cursorPosition >= source.size()) {
+            return EOFToken;
+        }
+
+        if (in(peek(), "\n")) {
+            advance();
+            currentState = State::Code;
+            if (cursorPosition >= source.size()) {
+                return EOFToken;
+            }
+            return handleCodeState();
+        }
+
+        return handleDirectiveState();
+    }
+
+    return EOFToken;
 }
--- src/engine/materials/glslPreprocessor/PreprocessorLexer.hpp
+++ src/engine/materials/glslPreprocessor/PreprocessorLexer.hpp
@@ -3,20 +3,22 @@
 
 #include <string>
 #include <vector>
+#include <regex>
 
 class PreprocessorLexer {
 public:
     enum class TokenType {
-        Code,
-        DirectiveMarker,
-        Identifier,
-        OpenBracket,
-        ClosedBracket,
-        Dot,
-        Float,
-        Integer,
+        Number,
         String,
-        EndOfFile
+        Identifier,
+        Dot,
+        LBracket,
+        RBracket,
+        Comma,
+        DirectiveMarker,
+        Code,
+        EndOfFile,
+        Unknown,
     };
 
     struct Token {
@@ -26,17 +28,47 @@ public:
         size_t column;
         size_t position;
     };
-private:
+
+    enum class State {
+        Code,
+        Directive
+    };
+
+    struct Rule {
+        TokenType type;
+        std::regex pattern;
+    };
+
     const std::string directiveMarker = "//@";
 
+    const std::vector<Rule> directiveRules = {
+        { TokenType::Number,     std::regex(R"(^(\d+\.\d+|\d+))") },
+        { TokenType::String,     std::regex(R"(^"([^"\\]|\\.)*")") },
+        { TokenType::Identifier, std::regex(R"(^([a-zA-Z_]\w*))") },
+        { TokenType::Dot,        std::regex(R"(^\.)") },
+        { TokenType::LBracket,   std::regex(R"(^\()") },
+        { TokenType::RBracket,   std::regex(R"(^\))") },
+        { TokenType::Comma,      std::regex(R"(^,)") } 
+    };
+private:
     std::string_view source;
     size_t cursorPosition = 0;
     size_t line = 0;
     size_t column = 0;
-    bool inDirective = false;
+
+    State currentState = State::Code;
+    const Token EOFToken {
+        .type = PreprocessorLexer::TokenType::EndOfFile,
+        .value = "",
+        .line = line,
+        .column = column, 
+        .position = cursorPosition
+    };
+    const int unknownTokenSize = 1;
 
     char peek() const;
     void advance();
+    void advance(int n);
 
     bool in(char c, const std::string& str);
 
@@ -44,6 +76,9 @@ private:
     bool isAlpha(char c);
     bool isDigit(char c);
     bool isAlphaDigit(char c);
+
+    PreprocessorLexer::Token handleCodeState();
+    PreprocessorLexer::Token handleDirectiveState();
 public:
     PreprocessorLexer(std::string_view _source) : source(_source) {}
 
