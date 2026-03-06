a2a49b5e188453dd4beee3a29ac1a401cb29934e
Minuta18 <ig.samsonov10@yandex.ru>
2026-03-06 20:19:16 +0300

    Idk preprocessor setup

---
+++ src/engine/materials/glslPreprocessor/Preprocessor.hpp
@@ -0,0 +1,14 @@
+#ifndef ENGINE_MATERIALS_GLSLPREPROCESSOR_PREPROCESSOR_H_
+#define ENGINE_MATERIALS_GLSLPREPROCESSOR_PREPROCESSOR_H_
+
+#include <string>
+
+#include "PreprocessorLexer.hpp"
+
+class Preprocessor {
+    PreprocessorLexer lexer;
+public:
+    Preprocessor();
+};
+
+#endif // ENGINE_MATERIALS_GLSLPREPROCESSOR_PREPROCESSOR_H_
