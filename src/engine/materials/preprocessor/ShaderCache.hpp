3cbd469a777f6a12853c222f38c9db52cef86882
Minuta18 <ig.samsonov10@yandex.ru>
2026-03-30 19:37:17 +0300

    GLSLPreprocessor -> preprocessor, this name is far more accurate

---
+++ src/engine/materials/preprocessor/ShaderCache.hpp
@@ -0,0 +1,13 @@
+#ifndef ENGINE_MATERIALS_PREPROCESSOR_SHADERCACHE_H_
+#define ENGINE_MATERIALS_PREPROCESSOR_SHADERCACHE_H_
+
+#include <unordered_map>
+
+#include "../../project/VirtualPath.hpp"
+#include "PreprocessorParser.hpp"
+
+class ShaderCache {
+
+};
+
+#endif // ENGINE_MATERIALS_GLSLPREPROCESSOR_SHADERCACHE_H_
