dd2ccb58e0d60f0f76fe975ab7d89f394624fd3f
Minuta18 <ig.samsonov10@yandex.ru>
2026-04-03 16:28:30 +0300

    WIP: Cached shader source

--- src/engine/materials/preprocessor/ShaderCache.hpp
+++ src/engine/materials/preprocessor/ShaderCache.hpp
@@ -1,13 +1,45 @@
-#ifndef ENGINE_MATERIALS_PREPROCESSOR_SHADERCACHE_H_
-#define ENGINE_MATERIALS_PREPROCESSOR_SHADERCACHE_H_
+#ifndef ENGINE_MATERIALS_PREPROCESSOR_SHADERSOURCECACHE_H_
+#define ENGINE_MATERIALS_PREPROCESSOR_SHADERSOURCECACHE_H_
 
 #include <unordered_map>
+#include <memory>
+#include <string>
+#include <filesystem>
 
 #include "../../project/VirtualPath.hpp"
 #include "PreprocessorParser.hpp"
+#include "../../project/FilesystemAbstraction.hpp"
 
-class ShaderCache {
+class CachedShaderSource {
+public:
+    struct Metadata {
+        VirtualPath sourceFilePath;
+        std::filesystem::file_time_type sourceFileLastEdit;
+    };
 
+    std::string processedSource;
+    std::string name;
+    std::string section;
+    Metadata meta;
 };
 
-#endif // ENGINE_MATERIALS_GLSLPREPROCESSOR_SHADERCACHE_H_
+class ShaderSourceCache {
+    FilesystemAbstraction& filesystem;
+
+    VirtualPath cachePath;
+public:
+    ShaderSourceCache(
+        FilesystemAbstraction& _abstraction, 
+        VirtualPath cachePath
+    );
+
+    bool exists(std::string name, std::string section);
+    CachedShaderSource get(std::string name, std::string section);
+    void set(CachedShaderSource shader);
+
+    VirtualPath constructPath(std::string, std::string section);
+
+    void clear();
+};
+
+#endif // ENGINE_MATERIALS_PREPROCESSOR_SHADERSOURCECACHE_H_
