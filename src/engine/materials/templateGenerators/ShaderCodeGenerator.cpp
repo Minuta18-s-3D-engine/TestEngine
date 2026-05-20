c2506c3740ba02cd34fa5a73a03aa1b6531669b2
Minuta18 <ig.samsonov10@yandex.ru>
2026-05-21 00:39:38 +0300

    WIP: ShaderCodeGenerator 2: added methods for generating sampler definitions, getters and material struct unpacking

--- src/engine/materials/templateGenerators/ShaderCodeGenerator.cpp
+++ src/engine/materials/templateGenerators/ShaderCodeGenerator.cpp
@@ -1,12 +1,18 @@
 #include "ShaderCodeGenerator.hpp"
 
 ShaderCodeGenerator::ShaderCodeGenerator() : 
-    templateEngine("core://templates", parser) {}
+    templateEngine("core://assets/templates", parser) {}
 
 std::string ShaderCodeGenerator::generateEngineGlobals() const {
     return templateEngine.render(
-        "core://templates/shaders/components/engineGlobals.glsl"
+        "shaders/components/engineGlobals.glsl"
     );
+}
+
+std::string ShaderCodeGenerator::getIndentStr(uint32_t indent) const {
+    std::string indentStr = "";
+    for (int i = 0; i < indent; ++i) indentStr += " ";
+    return indentStr;
 }
 
 std::string ShaderCodeGenerator::generateMaterialProperties(
@@ -15,25 +21,18 @@ std::string ShaderCodeGenerator::generateMaterialProperties(
     const auto& layout = mat.getLayout();
     const auto& props = layout.getPropertyOrder();
     
-    std::string indentStr = "";
-    for (int i = 0; i < indent; ++i) indentStr += " ";
+    
 
     std::stringstream result;
     for (const std::string& name : props) {
         const auto& propertyInfo = layout.getPropertyInfo(name);
 
-        result << indentStr;
+        result << getIndentStr(indent);
         result << MaterialLayout::getGLSLType(propertyInfo.type) << " ";
         result << name + ";\n";
     }
 
     return result.str();
-}
-
-std::string ShaderCodeGenerator::generateMaterialUnpacker(
-    const Material& mat, uint32_t indent = 4
-) const {
-    
 }
 
 std::string ShaderCodeGenerator::getGLSLSamplerName(
@@ -93,4 +92,198 @@ std::string ShaderCodeGenerator::generateSamplerGetters(
     }
 
     return result.str();
+}
+
+std::string ShaderCodeGenerator::generateBase(uint32_t idx) const {
+    return "b_MaterialData[u_CurrentMaterialStartId + " + std::to_string(idx) 
+        + "]";
+}
+
+std::string ShaderCodeGenerator::unpackInt(const std::string& base) const {
+    return "int(" + base + ")";
+}
+
+std::string ShaderCodeGenerator::unpackUint(const std::string& base) const {
+    return "(" + base + ")";
+}
+
+std::string ShaderCodeGenerator::unpackInt64(int idx) const {
+    return "packInt2x32(" + unpackVecIndexes("ivec2", idx, idx + 1) + ")";
+}
+
+std::string ShaderCodeGenerator::unpackUint64(int idx) const {
+    return "packUint2x32(" + unpackVecIndexes("uvec2", idx, idx + 1) + ")";
+}
+
+std::string ShaderCodeGenerator::unpackFloat(const std::string& base) const {
+    return "uintBitsToFloat(" + base + ")";
+}
+
+std::string ShaderCodeGenerator::unpackBool(const std::string& base) const {
+    return "(" + base + " != 0U)";
+}
+
+std::string ShaderCodeGenerator::unpackVec(
+    const std::string& type, const std::vector<std::string>& values
+) const {
+    std::stringstream result;
+    result << type << "(";
+    for (int i = 0; i < values.size() - 1; ++i) {
+        result << values[i] << ", ";
+    }
+    result << values.back() << ")";
+    return result.str();
+}
+
+std::string ShaderCodeGenerator::unpackVecIndexes(
+    const std::string& type, const std::vector<int>& values
+) const {
+    std::vector<std::string> strValues;
+    strValues.reserve(values.size());
+    for (int index : values) {
+        strValues.push_back(generateBase(index));
+    }
+    return unpackVec(type, strValues);
+}
+
+std::string ShaderCodeGenerator::unpackVecIndexes(
+    const std::string& type, int minValue, int maxValue
+) const {
+    std::vector<int> values(maxValue - minValue + 1);
+    for (int i = minValue; i <= maxValue; ++i) {
+        values[i - minValue] = i;
+    }
+    return unpackVecIndexes(type, values);
+}
+
+std::string ShaderCodeGenerator::generateMaterialUnpacker(
+    const Material& mat, const std::string& prop, uint32_t indent = 4
+) const {
+    const auto& propInfo = mat.getLayout().getPropertyInfo(prop);
+    std::string base = generateBase(propInfo.offset);
+
+    std::string converterFunc = "";
+    switch (propInfo.type) {
+        case MaterialLayout::PropertyType::Int:
+            converterFunc = unpackInt(base);
+        case MaterialLayout::PropertyType::Uint:
+            converterFunc = unpackUint(base);
+        case MaterialLayout::PropertyType::Int64:
+            converterFunc = unpackInt64(propInfo.offset);
+        case MaterialLayout::PropertyType::Uint64:
+            converterFunc = unpackUint64(propInfo.offset);
+        case MaterialLayout::PropertyType::Float:
+            converterFunc = unpackFloat(base);
+        case MaterialLayout::PropertyType::Bool:
+            converterFunc = unpackBool(base);
+        case MaterialLayout::PropertyType::Vec2:
+            converterFunc = unpackVecIndexes(
+                "vec2", propInfo.offset, propInfo.offset + 1
+            );
+        case MaterialLayout::PropertyType::IVec2:
+            converterFunc = unpackVecIndexes(
+                "ivec2", propInfo.offset, propInfo.offset + 1
+            );
+        case MaterialLayout::PropertyType::UVec2:
+            converterFunc = unpackVecIndexes(
+                "uvec2", propInfo.offset, propInfo.offset + 1
+            );
+        case MaterialLayout::PropertyType::Vec3:
+            converterFunc = unpackVecIndexes(
+                "vec3", propInfo.offset, propInfo.offset + 2
+            );
+        case MaterialLayout::PropertyType::IVec3:
+            converterFunc = unpackVecIndexes(
+                "ivec3", propInfo.offset, propInfo.offset + 2
+            );
+        case MaterialLayout::PropertyType::UVec3:
+            converterFunc = unpackVecIndexes(
+                "uvec3", propInfo.offset, propInfo.offset + 2
+            );
+        case MaterialLayout::PropertyType::Vec4:
+            converterFunc = unpackVecIndexes(
+                "vec4", propInfo.offset, propInfo.offset + 3
+            );
+        case MaterialLayout::PropertyType::IVec4:
+            converterFunc = unpackVecIndexes(
+                "ivec4", propInfo.offset, propInfo.offset + 3
+            );
+        case MaterialLayout::PropertyType::UVec4:
+            converterFunc = unpackVecIndexes(
+                "uvec4", propInfo.offset, propInfo.offset + 3
+            );
+        case MaterialLayout::PropertyType::Mat2:
+            converterFunc = unpackVecIndexes(
+                "mat2", propInfo.offset, propInfo.offset + 3
+            ); 
+        case MaterialLayout::PropertyType::Mat3:
+            converterFunc = unpackVecIndexes(
+                "mat3", propInfo.offset, propInfo.offset + 8
+            ); 
+        case MaterialLayout::PropertyType::Mat4:
+            converterFunc = unpackVecIndexes(
+                "mat4", propInfo.offset, propInfo.offset + 15
+            ); 
+        default:
+            converterFunc = "(0 /* Unsupported type */)";
+    }
+
+    std::stringstream result;
+    result << getIndentStr(indent) << "currentMaterial." << prop;
+    result << " = " << converterFunc << ";\n";
+    return result.str();
+}
+
+std::string ShaderCodeGenerator::generateUnpackerFunc(
+    const Material& mat
+) const {
+    const auto& properties = mat.getLayout().getPropertyOrder();
+    
+    std::stringstream result;
+    for (const auto& prop : properties) {
+        result << generateMaterialUnpacker(
+            mat, prop
+        );
+    }
+    return result.str();
+}
+
+std::string ShaderCodeGenerator::generateMaterialDefinition(
+    const Material& mat
+) const {
+    TemplateArguments args;
+    args.set("material_name", mat.getName());
+    args.set("sampler_definitions", generateSamplerDefinitions(mat));
+    args.set("sampler_getters", generateSamplerGetters(mat));
+    args.set("unpack_lines", generateUnpackerFunc(mat));
+
+    return templateEngine.render(
+        "shaders/materialDefinition.glsl", args
+    );
+}
+
+std::string ShaderCodeGenerator::generateShader(
+    const Material& mat, const std::string& type, const std::string& userCode
+) const {
+    TemplateArguments engineGlobalsArgs;
+    engineGlobalsArgs.set(
+        "material_definition", generateMaterialDefinition(mat)
+    );
+    std::string engineGlobals = templateEngine.render(
+        "shaders/generalShaderTemplate.glsl", engineGlobalsArgs
+    );
+
+    TemplateArguments args;
+    args.set("engine_globals", engineGlobals);
+    args.set(
+        "shader_specific_globals", 
+        templateEngine.render("shaders/components" + type + "Globals.glsl")
+    );
+    args.set(
+        "generated_header", 
+        templateEngine.render("shaders/headers/materialLoaderHeader.glsl")
+    );
+    args.set("user_code", userCode);
+
+    return templateEngine.render("shaders/generalShaderTemplate.glsl", args);
 }
--- src/engine/materials/templateGenerators/ShaderCodeGenerator.hpp
+++ src/engine/materials/templateGenerators/ShaderCodeGenerator.hpp
@@ -12,11 +12,30 @@ class ShaderCodeGenerator {
     TemplateParser parser;
     TemplateEngine templateEngine;
 
-    std::string generateEngineGlobals() const;
-    std::string generateMaterialProperties(
-        const Material& mat, uint32_t indent = 4
+    std::string getIndentStr(uint32_t indent) const;
+    std::string generateBase(uint32_t idx) const;
+    std::string unpackInt(const std::string& base) const;
+    std::string unpackUint(const std::string& base) const;
+    std::string unpackInt64(int idx) const;
+    std::string unpackUint64(int idx) const;
+    std::string unpackFloat(const std::string& base) const;
+    std::string unpackBool(const std::string& base) const;
+    std::string unpackVec(
+        const std::string& type, const std::vector<std::string>& values
+    ) const;
+    std::string unpackVecIndexes(
+        const std::string& type, const std::vector<int>& values
+    ) const;
+    std::string unpackVecIndexes(
+        const std::string& type, int minValue, int maxValue
     ) const;
     std::string generateMaterialUnpacker(
+        const Material& mat, const std::string& prop, uint32_t indent = 4
+    ) const;
+    std::string generateUnpackerFunc(const Material& mat) const;
+
+    std::string generateEngineGlobals() const;
+    std::string generateMaterialProperties(
         const Material& mat, uint32_t indent = 4
     ) const;
     std::string getGLSLSamplerName(const std::string& name) const;
@@ -29,7 +48,11 @@ class ShaderCodeGenerator {
 public:
     ShaderCodeGenerator();
 
-    
+    std::string generateMaterialDefinition(const Material& mat) const;
+    std::string generateShader(
+        const Material& mat, const std::string& type, 
+        const std::string& userCode
+    ) const;
 };
 
 #endif // ENGINE_MATERIALS_TEMPLATEGENERATORS_SHADERCODEGENRATOR_H_
