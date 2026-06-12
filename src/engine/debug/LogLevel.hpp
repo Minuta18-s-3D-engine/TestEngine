239f4c8374a187a02852892b48cb23d143527bea
Minuta18 <ig.samsonov10@yandex.ru>
2026-06-12 23:25:15 +0300

    WIP: Logger

---
+++ src/engine/debug/LogLevel.hpp
@@ -0,0 +1,12 @@
+#ifndef ENGINE_DEBUG_LOGLEVEL_H_
+#define ENGINE_DEBUG_LOGLEVEL_H_
+
+enum class LogLevel {
+    DEBUG,
+    INFO,
+    WARNING,
+    ERROR,
+    FATAL
+};
+
+#endif // ENGINE_DEBUG_LOGLEVEL_H_
---
+++ src/engine/debug/Logger.hpp
@@ -0,0 +1,9 @@
+#ifndef ENGINE_DEBUG_LOGGER_H_
+#define ENGINE_DEBUG_LOGGER_H_
+
+class Logger {
+public:
+    
+};
+
+#endif // ENGINE_DEBUG_LOGGER_H_
