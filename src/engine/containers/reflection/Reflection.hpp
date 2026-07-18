bcb1b0bf7c9d63e6e8f24854be4ba8e197dfe54d
Minuta18 <ig.samsonov10@yandex.ru>
2026-07-18 16:22:39 +0300

    Reflection system

---
+++ src/engine/containers/reflection/Reflection.hpp
@@ -0,0 +1,46 @@
+#ifndef ENGINE_CONTAINERS_REFLECTION_REFLECTION_HPP_
+#define ENGINE_CONTAINERS_REFLECTION_REFLECTION_HPP_
+
+#include <string_view>
+#include <optional>
+
+namespace Reflection {
+
+template <typename Class, typename T>
+struct FieldDef {
+    std::string_view name;
+    T Class::* member;
+    std::optional<T> defaultValue = std::nullopt;
+    bool required = true;
+};
+
+template <typename Class, typename T>
+constexpr auto Field(std::string_view name, T Class::* member) {
+    return FieldDef<Class, T>{ name, member, std::nullopt, true };
+}
+
+template <typename Class, typename T>
+constexpr auto Field(
+    std::string_view name, T Class::* member, const T& defaultValue
+) {
+    return FieldDef<Class, T>{ name, member, defaultValue, false };
+}
+
+template <typename T>
+struct Meta {
+    static constexpr bool isMapped = true;
+};
+
+template <typename E>
+constexpr auto EnumVal(std::string_view name, E val) {
+    return std::pair<std::string_view, E>{ name, val };
+}
+
+template <typename T>
+struct EnumMeta {
+    static constexpr bool isMapped = true;  
+};
+
+} // namespace Reflection
+
+#endif // ENGINE_CONTAINERS_REFLECTION_REFLECTION_HPP_
